ac7c282ee4870de247b666c8b7c31f3f7a3726cb
wreed3rd <wreed3rd@gmail.com>
2022-03-22 00:12:25 -0400

    Assignment 4 upload

--- queries.sql
+++ queries.sql
@@ -1,7 +1,22 @@
 -- Part 1: Test it with SQL
+CREATE TABLE job (
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+employer VARCHAR(255),
+name VARCHAR(255),
+skills VARCHAR(255)
+);
 
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+Where location = "St. Louis City";
 
 -- Part 3: Test it with SQL
+DROP TABLE job;
 
 -- Part 4: Test it with SQL
+SELECT *
+FROM Skill
+LEFT JOIN job_skills on skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,49 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(message = "Please enter a location")
+    @NotBlank(message = "Your location field can not be left blank")
+    @NotEmpty(message = "Your location field can not be empty")
+    private String location;
 
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer(String location, List<Job> jobs) {
+        this.location = location;
+        this.jobs = jobs;
+    }
+
+    public Employer(){
+
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
